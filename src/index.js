1219bea3cb28855dcb224d2f7c32074d25111696
ytorii <metalwarrior_type_jp@hotmail.com>
2018-09-10 10:42:22 +0900

    Remove style and css from bunlde to avoid breaking style of the site

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-social/bootstrap-social.css";
 import App from './App';
 
 ReactDOM.render(

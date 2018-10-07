7e845575ad91799b21bbb8648ae7e6fadecf371f
ytorii <metalwarrior_type_jp@hotmail.com>
2018-10-07 23:56:28 +0900

    Rename tebukuro button class for clalification

--- src/components/TebukuroButton.js
+++ src/components/TebukuroButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ThumbUp from '../svg/baseline-thumb_up-24px.svg'
 
 const TebukuroButton = props => (
-  <button className="btn" onClick={props.onClick}>
+  <button className="tebukuro-button" onClick={props.onClick}>
     <ThumbUp/>
     <span>Register with <strong>Tebukuro</strong></span>
   </button>
